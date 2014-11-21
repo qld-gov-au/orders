17fe9e4fd4dffa39fb770d2cb9bac9cc2fb87719
steven_baker <steven.baker@justice.qld.gov.au>
2014-11-21 11:39:58 +1000

    removing old hard coded password so it is stored outside the application

--- src/main/java/au/gov/qld/pub/orders/service/EncryptionConfiguration.java
+++ src/main/java/au/gov/qld/pub/orders/service/EncryptionConfiguration.java
@@ -1,11 +1,24 @@
 package au.gov.qld.pub.orders.service;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jasypt.encryption.pbe.config.SimplePBEConfig;
 
 public class EncryptionConfiguration extends SimplePBEConfig {
-    public EncryptionConfiguration() {
+    private static final String ALGORITHM = "PBEWITHSHA256AND128BITAES-CBC-BC";
+    private static final String KEY_FILENAME = ".orders.key";
+    private static final String PASSWORD_LOC = System.getProperty("user.home") + File.separator + KEY_FILENAME; 
+    
+    public EncryptionConfiguration() throws IOException {
         super();
-        setPassword("tK8diX83m6zBt6nX4Ym0Fl");
+        String password = FileUtils.readFileToString(new File(PASSWORD_LOC)).trim();
+        
+        setProvider(new BouncyCastleProvider());
+        setAlgorithm(ALGORITHM);
+        setPassword(password);
     }
     
 }
