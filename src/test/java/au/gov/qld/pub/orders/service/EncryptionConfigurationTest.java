12c37d018f758a399de25dbd9915222f5c801e0a
Steven Baker <steven.baker@justice.qld.gov.au>
2014-12-08 09:27:13 +1000

    testing key exists and with correct key for testing

---
+++ src/test/java/au/gov/qld/pub/orders/service/EncryptionConfigurationTest.java
@@ -0,0 +1,24 @@
+package au.gov.qld.pub.orders.service;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+
+public class EncryptionConfigurationTest {
+	@Test
+	public void loadEncryptionKey() throws Exception {
+		String location = System.getProperty("user.home") + File.separator + ".orders.key";
+		File keyFile = new File(location);
+		if (!keyFile.exists()) {
+			FileUtils.write(keyFile, "testkey");
+		}
+		
+		EncryptionConfiguration configuration = new EncryptionConfiguration();
+		assertThat(configuration.getPassword(), is("testkey"));
+	}
+}
