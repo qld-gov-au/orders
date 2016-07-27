feae6e8a273bdccb3ada80ea1028590cc45727ff
Steven Baker <steven.baker@justice.qld.gov.au>
2016-07-27 14:32:31 +1000

    Support for extra notification in an overlay. e.g. message to JMS.

---
+++ orders-core/src/main/java/au/gov/qld/pub/orders/service/AdditionalNotificationService.java
@@ -0,0 +1,15 @@
+package au.gov.qld.pub.orders.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+
+public interface AdditionalNotificationService {
+
+	void notifedPaidOrder(String id, Date created, String paid, String receipt, String cartId,
+			Map<String, String> customerDetailsMap, Map<String, String> deliveryDetailsMap, List<Map<String, String>> fieldMaps);
+
+	void notifedPaidNoticeToPay(String id, Date notifiedAt, String receiptNumber, long amount, long amountGst, String description, String paymentInformationId);
+
+}
---
+++ orders-core/src/main/java/au/gov/qld/pub/orders/service/StubAdditionalNotificationService.java
@@ -0,0 +1,24 @@
+package au.gov.qld.pub.orders.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+
+public class StubAdditionalNotificationService implements AdditionalNotificationService {
+
+	@Override
+	public void notifedPaidOrder(String id, Date created, String paid,
+			String receipt, String cartId,
+			Map<String, String> customerDetailsMap,
+			Map<String, String> deliveryDetailsMap,
+			List<Map<String, String>> fieldMaps) {
+	}
+
+	@Override
+	public void notifedPaidNoticeToPay(String id, Date notifiedAt,
+			String receiptNumber, long amount, long amountGst,
+			String description, String paymentInformationId) {
+	}
+
+}
