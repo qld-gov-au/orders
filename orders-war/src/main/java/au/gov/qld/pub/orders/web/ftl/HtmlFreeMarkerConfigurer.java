bbb0ee870759e7291def8b40edfb61251fac5b83
Steven Baker <steven.baker@justice.qld.gov.au>
2018-03-19 08:59:07 +1000

    fixing npe

--- orders-war/src/main/java/au/gov/qld/pub/orders/web/ftl/HtmlFreeMarkerConfigurer.java
+++ orders-war/src/main/java/au/gov/qld/pub/orders/web/ftl/HtmlFreeMarkerConfigurer.java
@@ -4,14 +4,12 @@ import java.util.List;
 
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 
-import freemarker.cache.NullCacheStorage;
 import freemarker.cache.TemplateLoader;  
   
 public class HtmlFreeMarkerConfigurer extends FreeMarkerConfigurer {  
   
     @Override  
     protected TemplateLoader getAggregateTemplateLoader(List<TemplateLoader> templateLoaders) {  
-    	getConfiguration().setCacheStorage(new NullCacheStorage());
         return new HtmlTemplateLoader(super.getAggregateTemplateLoader(templateLoaders));  
     }  
 }
