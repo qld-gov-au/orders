817cda3e059898a75aa6a359f117923b5d409cc3
william dutton <will.dutt@gmail.com>
2020-05-26 12:21:58 +1000

    update dependancies due to github warnings

--- orders-war/src/main/java/au/gov/qld/pub/orders/web/OrderController.java
+++ orders-war/src/main/java/au/gov/qld/pub/orders/web/OrderController.java
@@ -17,7 +17,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.collections.EnumerationUtils;
+import org.apache.commons.collections4.EnumerationUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
--- orders-war/src/main/java/au/gov/qld/pub/orders/web/OrderWithFileController.java
+++ orders-war/src/main/java/au/gov/qld/pub/orders/web/OrderWithFileController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.collections.EnumerationUtils;
+import org.apache.commons.collections4.EnumerationUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
